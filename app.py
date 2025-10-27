4308d5a2566344de8890d8d4d2a2dde34566b785
sander <sander_net@bk.ru>
2025-10-27 10:27:54 +0800

    argumented starting app

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-# Загружаем и валидируем конфигурацию
-# ====================================================
-from src.config.config import config
+import argparse
 
-# Логирование
-# ====================================================
-from src.utils.logger import get_logger
-logger = get_logger(__name__)
-logger.info("Конфигурация успешно загружена и прошла валидацию.")
+
 
 
 # ====================================================
 # Основная функция
 # ====================================================
 def main():
+    parser = argparse.ArgumentParser(description='Программа алготрейдинга с бэктестером и загрузкой данных.')
     
-    # logger.info("Запуск основного конвейера получения и сохранения исторических данных...")
-    # from src.logical.data_fetcher.data_pipeline import run_data_update_pipeline
-    # run_data_update_pipeline()
-    # logger.info("Конвейер завершил работу.")
+    # Добавляем параметр --loaddata
+    parser.add_argument(
+        '--loaddata',
+        action='store_true',  # Флаг (без значения)
+        help='Загрузить или обновить исторические данные перед запуском бэктеста'
+    )
+    
+    
+    
+    args = parser.parse_args()
+    
+    # Загружаем и валидируем конфигурацию
+    # ====================================================
+    from src.config.config import config
+
+    # Логирование
+    # ====================================================
+    from src.utils.logger import get_logger
+    logger = get_logger(__name__)
+    logger.info("Конфигурация успешно загружена и прошла валидацию.")
+    # Проверяем параметры
+    if args.loaddata:
+        logger.info("Загрузка и обновление исторических данных...")
+        from src.logical.data_fetcher.data_pipeline import run_data_update_pipeline
+        run_data_update_pipeline()
+        logger.info("Загрузка и обновление исторических данных завершены.")
     
     logger.info("Запуск бэктестера с локальными данными...")
     from src.logical.backtester.backtester import run_local_backtest
